2fa8381b8ce9aea04c4a080c5c87a96f2a54efef
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-10-27 20:54:29 +0100

    feat: switch from popup to devtool

--- src/background.ts
+++ src/background.ts
@@ -5,3 +5,8 @@ console.log("Hello from the background!");
 browser.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
 });
+
+Browser.devtools.panels.create("TypedRes",
+    "/icon-with-shadow.svg",
+    "src/popup.html",
+);
