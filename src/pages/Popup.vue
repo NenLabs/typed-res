6afdc9e9869d5f90807f64d5501e22891b9c8f29
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-11-07 15:09:30 +0100

    style: restyle popup

--- src/pages/Popup.vue
+++ src/pages/Popup.vue
@@ -14,20 +14,18 @@ Browser.devtools.panels.create("TypedRes",
 </script>
 
 <template>
-  <div>
-    <img src="/icon-with-shadow.svg" />
-    <h1>TypedRes</h1>
-    <p>
-      Template: <code>vue-ts</code> <code>tailwindcss</code>
-    </p>
+  <div class="h-screen w-full">
+    <div class="p-5">
+      <img src="/icon-with-shadow.svg" />
+      <h1 class="">TypedRes</h1>
+      <p>Template: <code>vue-ts</code> <code>tailwindcss</code></p>
+    </div>
   </div>
 </template>
 
 <style>
 html,
 body {
-  width: 300px;
-  height: 400px;
   padding: 0;
   margin: 0;
 }
@@ -36,37 +34,8 @@ body {
   background-color: rgb(36, 36, 36);
 }
 
-body > div {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  align-items: center;
-  justify-content: center;
-}
-
 img {
   width: 200px;
   height: 200px;
-}
-
-h1 {
-  font-size: 18px;
-  color: white;
-  font-weight: bold;
-  margin: 0;
-}
-
-p {
-  color: white;
-  opacity: 0.7;
-  margin: 0;
-}
-
-code {
-  font-size: 12px;
-  padding: 2px 4px;
-  background-color: #ffffff24;
-  border-radius: 2px;
 }
 </style>
